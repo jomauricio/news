c377c6f3d8bf1b198a4d8ea2c50efa30729299b7
atividadesinfantil <georgiodesignweb@gmail.com>
2023-03-02 16:35:48 -0300

    Modificado admin

--- noticias/admin.py
+++ noticias/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from .models import Autor, Noticia
 
 # Register your models here.
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ["nome", "cpf", "email"]
+    search_fields = ["nome", "email"]
 
-admin.site.register(Autor)
-admin.site.register(Noticia)
+@admin.register(Noticia)
+class NoticiaAdmin(admin.ModelAdmin):
+    list_display = ["titulo", "data_pub", "autor"]
+    search_fields = ["titulo", "autor__nome"]
+    autocomplete_fields = ["autor"]
+
 
--- noticias/models.py
+++ noticias/models.py
@@ -4,20 +4,26 @@ from django.db import models
 
 class Autor(models.Model):
 
-    nome = models.CharField(max_length=250)
-    email = models.EmailField()
-    cpf = models.CharField(max_length=15)
-    data_nascimento = models.DateField()
+    nome = models.CharField("Nome", max_length=250)
+    email = models.EmailField("Email")
+    cpf = models.CharField("CPF", max_length=15)
+    data_nascimento = models.DateField("Data de nascimento")
 
     def __str__(self):
-        return "{} - {}".format(self.nome, self.email)
+        return "{}".format(self.nome)
+    
+    class Meta:
+        verbose_name_plural = "Autores"
 
 class Noticia(models.Model):
 
-    titulo = models.CharField(max_length=100)
-    conteudo = models.TextField()
-    data_pub = models.DateField()
-    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, related_name='noticias')
+    titulo = models.CharField("Titulo", max_length=100)
+    conteudo = models.TextField("Conteudo")
+    data_pub = models.DateField("Data de publicação")
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, related_name='noticias', verbose_name="Autor")
 
     def __str__(self):
-        return "{} - {}".format(self.titulo, self.autor)
+        return "{}".format(self.titulo)
+
+    class Meta:
+        verbose_name_plural = "Noticias"
